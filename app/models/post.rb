988bfad024a89d040c141bbc629fc6ee673c0949
JimmyTsai <qaz3182@gmail.com>
2018-07-08 22:45:53 +0800

    add group amount

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :user
-  	belongs_to :group
-  	validates :content, presence: true
+  belongs_to :group
+  validates :content, presence: true
 
-  	scope :recent, -> { order("created_at DESC")}
+  scope :recent, -> { order("created_at DESC")}
 end
